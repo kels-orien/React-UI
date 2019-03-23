bc0e1bb4078f4ea64c7b78d794fc927cd0972639
kels-orien <kelvinorien@gmail.com>
2019-03-23 18:16:08 +0100

    fifth commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+import "./Form.scss";
 
-const Form = () => {
-  return <div />;
+export default class Form extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("submit clicked");
+  };
+  render() {
+    const { buttonText, title } = this.props;
+    return (
+      <div className="login-form">
+        <form onSubmit={this.handleSubmit}>
+          <h1>{title}</h1>
+          <input type="text" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
+          <Button className="button-one" type="submit">
+            {buttonText}
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+Form.defaultProps = {
+  buttonText: "Submit",
+  title: "Login"
 };
 
-export default Form;
+Form.propTypes = {
+  buttonText: PropTypes.string,
+  title: PropTypes.string
+};
