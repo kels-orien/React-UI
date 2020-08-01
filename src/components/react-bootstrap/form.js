492410f08eb362a5e9193f5b9cd15ae37dea7001
Kohwo Orien <kowo4real@yahoo.co.uk>
2020-08-01 21:59:17 +0100

    Regex fix

--- src/components/react-bootstrap/form.js
+++ src/components/react-bootstrap/form.js
@@ -7,31 +7,35 @@ const HookForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-         <input
+      <input
         name="firstname"
         ref={register({
-            required: "Required",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: "invalid firstname",
-            },
-          })}
+          required: "Required",
+          pattern: {
+            value: /^[A-Za-z\. -]+$/i,
+            message: "invalid firstname",
+          },
+        })}
       />
+      <span class="floating-label">First Name</span>
+
       {errors.firstname && errors.firstname.message}
 
       <input
         name="lastname"
+        placeholder="Last Name"
         ref={register({
-            required: "Required",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: "invalid lastname",
-            },
-          })}
+          required: "Required",
+          pattern: {
+            value: /^[A-Za-z\. -]+$/i,
+            message: "invalid lastname",
+          },
+        })}
       />
       {errors.lastname && errors.lastname.message}
       <input
         name="email"
+        placeholder="Email Address"
         ref={register({
           required: "Required",
           pattern: {
@@ -40,11 +44,19 @@ const HookForm = () => {
           },
         })}
       />
-      {errors.email && errors.email.message}
+      <input
+        name="number"
+        ref={register({
+          required: "Required",
+          pattern: {
+            value: /^[0]\d{10}$/i,
+            message: "invalid phone number",
+          },
+        })}
+      />
+      {errors.number && errors.email.number}
 
-     
-
-      <button type="submit">Submit</button>
+      <button type="submit">Pay Now</button>
     </form>
   );
 };
